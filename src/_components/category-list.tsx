2b572a7a22d2c0a809f01b086f354a979898bba8
lukaswyver <lucas_sin5@hotmail.com>
2024-05-04 19:26:28 -0300

    feat: add promo banner to homepage

--- src/_components/category-list.tsx
+++ src/_components/category-list.tsx
@@ -5,7 +5,7 @@ const CategoryList = async () => {
   const categories = await db.category.findMany({});
 
   return (
-    <div className="grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
+    <div className="grid grid-cols-2 gap-3">
       {categories.map((category) => (
         <CategoryItem category={category} key={category.id} />
       ))}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import CategoryList from "@/_components/category-list";
 import Header from "@/_components/header";
 import Search from "@/_components/search";
+import Image from "next/image";
 
 const Home = () => {
   return (
@@ -11,6 +12,18 @@ const Home = () => {
       </div>
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+
+      <div className="px-5 pt-6">
+        <Image
+          src="/promo-banner.png"
+          alt="Até 30% de desconto em pizzas"
+          className="h-auto w-full object-contain"
+          sizes="100vh"
+          quality={100}
+          height={0}
+          width={0}
+        />
       </div>
     </>
   );
