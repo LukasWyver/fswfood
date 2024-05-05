5cc43d291fbbbfee2bc33f6a2a47814f8d114330
lukaswyver <lucas_sin5@hotmail.com>
2024-05-05 20:29:19 -0300

    feat: add product list and item components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 import CategoryList from "@/_components/category-list";
 import Header from "@/_components/header";
+import ProductList from "@/_components/product-list";
 import Search from "@/_components/search";
+import { Button } from "@/_components/ui/button";
+import { ChevronRightIcon } from "lucide-react";
+import { Metadata } from "next";
 import Image from "next/image";
+
+export const metadata: Metadata = {
+  title: "Home",
+};
 
 const Home = () => {
   return (
@@ -24,6 +32,21 @@ const Home = () => {
           height={0}
           width={0}
         />
+      </div>
+      <div className="space-y-4 pt-6">
+        <div className="flex items-center justify-between px-5">
+          <h2 className="font-semibold">Pedidos Recomendados</h2>
+
+          <Button
+            variant="ghost"
+            className="h-fit p-0 text-primary hover:bg-transparent"
+          >
+            Ver todos
+            <ChevronRightIcon size={16} />
+          </Button>
+        </div>
+
+        <ProductList />
       </div>
     </>
   );
